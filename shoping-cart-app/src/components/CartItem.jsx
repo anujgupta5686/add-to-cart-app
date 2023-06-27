951bf69d0c3cc685ff7d028bdfb83a168ffa1309
Anuj Kumar Gupta <73886648+anujgupta5686@users.noreply.github.com>
2023-06-27 08:49:40 +0530

    I have design fully add to cart app
    I have design fully add to cart application using ReactJS

---
+++ shoping-cart-app/src/components/CartItem.jsx
@@ -0,0 +1,36 @@
+import { FcDeleteDatabase } from "react-icons/fc";
+import { useDispatch } from "react-redux";
+import { remove } from "../redux/Slices/CartSlice";
+import { toast } from "react-hot-toast";
+const CartItem = ({ item, itemIndex }) => {
+  const dispatch = useDispatch();
+  const removeFromCart = () => {
+    dispatch(remove(item.id));
+    toast.success("Item Removed");
+  };
+  return (
+    <div>
+      <div className="flex border-b-2">
+        <div className="h-[180px]">
+          <img src={item.image} className="w-full h-full" />
+        </div>
+        <div>
+          <h1 className="text-gray-700 font-semibold text-lg text-left truncate w-40 mt-1">
+            {item.title}
+          </h1>
+          <h1 className="text-[10px] p-1 w-40 text-gray-400 font-normal text-left">
+            {item.description.split(" ").slice(0, 10).join(" ")}
+          </h1>
+          <div className="flex justify-between gap-12 items-center w-full mt-5">
+            <p className="text-green-600 font-semibold">{item.price}</p>
+            <div onClick={removeFromCart}>
+              <FcDeleteDatabase className="text-[2rem] cursor-pointer" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
---
+++ shoping-cart-app/src/components/Spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./SpinnerStyle.css";
+const Spinner = () => {
+  return (
+    <div>
+      <div class="spinner"></div>
+    </div>
+  );
+};
+
+export default Spinner;
